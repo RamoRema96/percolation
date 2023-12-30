42e91ea3bc540f2c561f633d7ba452d96f5c5b80
Omar Amer <oamer@keybiz.it>
2023-12-30 21:46:18 +0100

    add save path

--- src/main.py
+++ src/main.py
@@ -4,11 +4,12 @@ from visualization import visualize_fire_sequence
 
 # Parameter
 
-N = 30 # NxN is the dimension of the matrix
+N = 50 # NxN is the dimension of the matrix
 p = 0.6 # The density of the trees
 T = 20 # Time step montecarlo
 
 simulation = FireSimulation(N=N, p=p)
 configs = simulation.simulate_fire_spread(T)
-visualize_fire_sequence(configs)
-print(simulation.t)
+visualize_fire_sequence(configs, save_path=f"../image/animation_{N}_{p}_{simulation.t}.gif")
+
+print(f"Tempo di fine simulazione: {simulation.t}")
